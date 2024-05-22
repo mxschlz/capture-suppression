a5c3730eddd98755f03ec935a689437425e0c6ba
max <schulz.max5@gmail.com>
2024-05-22 15:37:52 +0200

    update

--- proportion_correct.py
+++ proportion_correct.py
@@ -8,15 +8,40 @@ plt.ion()
 fp = f"/home/max/data/behavior/all_subjects_additional_metrics.csv"
 df = pd.read_csv(fp)
 
+# Plot individual subject data as lines
+subjects = df['subject'].unique()
+
+# make subject-wise lineplots for average values in df
+df_mean = df.groupby(['subject', 'Singletonpres']).mean().reset_index()
+
 # remove outliers
-outlier_thresh = 1.3
-cleaned_rt = df.RT[df.RT < outlier_thresh]
+df.RT[df.RT > 2 * np.std(df.RT)] = np.nan  # drop values which deviate 2 standard deviations from the mean
+
+# make barplot
+barplot = sns.barplot(data=df, y=df.RT, hue="Singletonpres")
+
+# Get the positions of the bars for aligning lines correctly
+bar_positions = []
+for patch in barplot.patches:
+    bar_positions.append(patch.get_x() + patch.get_width() / 2)
 
 
-sns.barplot(data=df, y=df.RT, hue="Singletonpres")
+for subject in subjects:
+    subject_data = df_mean[df_mean['subject'] == subject]
+    # Aligning subject data with bar positions
+    x_positions = [bar_positions[1] if cond == 1 else bar_positions[0] for cond in subject_data['Singletonpres']]
+    plt.plot(x_positions, subject_data['RT'], marker='o', linestyle='-', color='grey', alpha=0.5)
+
 plt.savefig("/home/max/figures/rt_singleton_abs_vs_pres.png", dpi=400)
 plt.close()
 
-sns.barplot(data=df, y=df.correct, hue="Singletonpres")
+barplot = sns.barplot(data=df, y=df.correct, hue="Singletonpres")
+
+for subject in subjects:
+    subject_data = df_mean[df_mean['subject'] == subject]
+    # Aligning subject data with bar positions
+    x_positions = [bar_positions[1] if cond == 1 else bar_positions[0] for cond in subject_data['Singletonpres']]
+    plt.plot(x_positions, subject_data['correct'], marker='o', linestyle='-', color='grey', alpha=0.5)
+
 plt.savefig("/home/max/figures/correct_singleton_abs_vs_pres.png", dpi=400)
 plt.close()
--- target_singleton_distribution.py
+++ target_singleton_distribution.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
 import numpy as np
 plt.ion()
 
@@ -9,20 +8,16 @@ plt.ion()
 fp = f"/home/max/data/behavior/all_subjects_additional_metrics.csv"
 df = pd.read_csv(fp)
 
-crosstab = pd.crosstab(index=df.Singletonpos, columns=df.Targetpos)
-minmaxsclaer = preprocessing.MinMaxScaler()
-crosstab_normalized = minmaxsclaer.fit_transform(crosstab)
+crosstab = pd.crosstab(index=df.Singletonpos, columns=df.Targetpos, normalize=False)
+crosstab_flipped = np.flip(crosstab.values, axis=0)
 
-figure = sns.heatmap(crosstab_normalized[1:], annot=True, xticklabels=range(1, 6), yticklabels=range(1, 6))
-figure.set(xlabel="Singletonpos", ylabel="Targetpos")
-
-plt.savefig("/home/max/figures/singletonpos_targetpos_crosstab.png", dpi=400)
+crosstab.plot(kind="bar")
+plt.savefig("/home/max/figures/singletonpos_bar.png", dpi=400)
 plt.close()
 
-sns.displot(df.Singletonpos[df.Singletonpres == 1], kde=False, rug=True)
-plt.savefig("/home/max/figures/singletonpos_distribution.png", dpi=400)
-plt.close()
+crosstab = pd.crosstab(index=df.Singletontime, columns=df.Targettime, normalize=False)
+crosstab_flipped = np.flip(crosstab.values, axis=0)
 
-sns.displot(df.Targetpos, kde=False, rug=True)
-plt.savefig("/home/max/figures/targetpos_distribution.png", dpi=400)
+crosstab.plot(kind="bar")
+plt.savefig("/home/max/figures/singletontime_bar.png", dpi=400)
 plt.close()
