f472311239547cf0f02dc75699e4d60ded0dd12f
Max Schulz <max.schulz10@web.de>
2024-10-14 16:35:34 +0200

    WIP

--- SPACEPRIME/preproc.py
+++ SPACEPRIME/preproc.py
@@ -9,11 +9,11 @@ data_path = f"/home/max/data/SPACEPRIME/sub-{subject_id}/eeg/"
 settings_path = "/home/max/data/settings/"
 # read raw fif
 raw = mne.io.read_raw_fif(data_path + "concatenated_raw.fif", preload=True)
+# add reference channel
+raw.add_reference_channels(["Fz"])
 # Add a montage to the data
 montage = mne.channels.read_custom_montage(settings_path + "AS-96_REF.bvef")
 raw.set_montage(montage)
-# add reference channel
-raw.add_reference_channels(["Fz"])
 # get events
 events, event_id = mne.events_from_annotations(raw)
 event_dict = EEG_TRIGGER_MAP
@@ -22,15 +22,15 @@ sample_rate = raw.info["sfreq"]
 # Make a copy of the data
 raw_copy = raw.copy()
 # filter raw
-raw_filt = raw.filter(1, 40)
+raw_filt = raw.filter(1, 40, n_jobs=-1)
 # cut epochs
 epochs = mne.Epochs(raw_filt, events=events, preload=True, tmin=-0.2, tmax=1.0)
 bad_chs = ["TP9"]
 epochs.info["bads"] = bad_chs
-epochs.interpolate_bads()
+# epochs.interpolate_bads()
 # average reference
 epochs.set_eeg_reference(ref_channels="average", projection=True)
 epochs.apply_proj()
 # apply ICA
-ica = mne.preprocessing.ICA(n_components=20)
+ica = mne.preprocessing.ICA(n_components=5)
 ica.fit(epochs)
