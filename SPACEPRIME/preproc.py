55d3ee1d752568902690ae34d4ebdaa70416f5bc
max <schulz.max5@gmail.com>
2024-10-18 13:19:58 +0200

    Update with ica-label

--- SPACEPRIME/preproc.py
+++ SPACEPRIME/preproc.py
@@ -21,8 +21,8 @@ raw.interpolate_bads()
 # average reference
 raw.set_eeg_reference(ref_channels="average")
 # Downsample because the computer crashes if sampled with 1000 Hz :-(
-raw = raw.resample(250)
-# Filter the data
+raw = raw.resample(500)
+# Filter the data. These values are needed for the CNN to label the ICs effectively
 raw_filt = raw.copy().filter(1, 100)
 # apply ICA
 ica = mne.preprocessing.ICA(method="infomax", fit_params=dict(extended=True))
@@ -30,8 +30,11 @@ ica.fit(raw_filt)
 #ica.plot_components(picks=range(20))
 #ica.plot_sources(raw_filt, picks=range(20))
 ic_labels = mne_icalabel.label_components(raw_filt, ica, method="iclabel")
-labels = ic_labels["labels"]
-exclude_idx = [idx for idx, label in enumerate(ic_labels["labels"]) if label not in ["brain", "other"]]
+exclude_idx = [idx for idx, (label, prob) in enumerate(zip(ic_labels["labels"], ic_labels["y_pred_proba"])) if label not in ["brain", "other"]]
+print(f"Excluding these ICA components: {exclude_idx}")
+# ica.apply() changes the Raw object in-place, so let's make a copy first:
+reconst_raw = raw.copy()
+ica.apply(reconst_raw, exclude=exclude_idx)
 # get events from annotations
 events, event_id = mne.events_from_annotations(raw)
 # rename the keys in the event_id
@@ -45,6 +48,7 @@ event_dict = {key: value for key, value in event_id.items() if value >= 9}
 # cut epochs
 epochs = mne.Epochs(raw_filt, events=events, event_id=event_dict, preload=True, tmin=-0.2, tmax=1.5, baseline=(None, 0))
 evokeds = epochs.average(by_event_type=True)
+# sort evokeds
 np_trials = [x for x in evokeds if int(x.comment) > 200 ]
 pp_trials = [x for x in evokeds if 200 > int(x.comment) > 100 ]
 c_trials = [x for x in evokeds if int(x.comment) < 100 ]
