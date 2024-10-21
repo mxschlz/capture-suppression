7d07c9e055e56a989328abbd09fd10e66e211dce
Max Schulz <max.schulz10@web.de>
2024-10-21 15:50:05 +0200

    WIP

--- SPACEPRIME/preproc.py
+++ SPACEPRIME/preproc.py
@@ -21,32 +21,34 @@ raw.interpolate_bads()
 # average reference
 raw.set_eeg_reference(ref_channels="average")
 # Downsample because the computer crashes if sampled with 1000 Hz :-(
-raw = raw.resample(500)
+raw = raw.resample(250)
 # Filter the data. These values are needed for the CNN to label the ICs effectively
 raw_filt = raw.copy().filter(1, 100)
 # apply ICA
 ica = mne.preprocessing.ICA(method="infomax", fit_params=dict(extended=True))
 ica.fit(raw_filt)
-#ica.plot_components(picks=range(20))
-#ica.plot_sources(raw_filt, picks=range(20))
 ic_labels = mne_icalabel.label_components(raw_filt, ica, method="iclabel")
-exclude_idx = [idx for idx, (label, prob) in enumerate(zip(ic_labels["labels"], ic_labels["y_pred_proba"])) if label not in ["brain", "other"]]
+exclude_idx = [idx for idx, (label, prob) in enumerate(zip(ic_labels["labels"], ic_labels["y_pred_proba"])) if label not in ["brain", "other"] and prob > 0.9]
 print(f"Excluding these ICA components: {exclude_idx}")
 # ica.apply() changes the Raw object in-place, so let's make a copy first:
 reconst_raw = raw.copy()
 ica.apply(reconst_raw, exclude=exclude_idx)
+# band pass filter
+reconst_raw_filt = reconst_raw.copy().filter(0.1, None).notch_filter([50, 100])
 # get events from annotations
 events, event_id = mne.events_from_annotations(raw)
 # rename the keys in the event_id
 renamed_event_id = {}
 for key, value in event_id.items():
-  new_key = key.replace('Stimulus/S', '').strip()  # Remove "Stimulus/S" and strip any spaces
-  renamed_event_id[new_key] = value
+	new_key = key.replace('Stimulus/S', '').strip()  # Remove "Stimulus/S" and strip any spaces
+	renamed_event_id[new_key] = value
 event_id = renamed_event_id
 # filter for values above 10
 event_dict = {key: value for key, value in event_id.items() if value >= 9}
 # cut epochs
-epochs = mne.Epochs(raw_filt, events=events, event_id=event_dict, preload=True, tmin=-0.2, tmax=1.5, baseline=(None, 0))
+epochs = mne.Epochs(reconst_raw_filt, events=events, event_id=event_dict, preload=True, tmin=-0.2, tmax=1.5, baseline=(None, 0))
+# adjust timeline of epochs
+times = epochs.get_data()[2]
 evokeds = epochs.average(by_event_type=True)
 # sort evokeds
 np_trials = [x for x in evokeds if int(x.comment) > 200 ]
