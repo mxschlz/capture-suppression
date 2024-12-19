55927f57f927426145785a3eeff9d75e6106405e
Max Schulz <max.schulz10@web.de>
2024-12-19 12:49:30 +0100

    Do not concatenate flanker data

--- SPACEPRIME/concatenate_raws.py
+++ SPACEPRIME/concatenate_raws.py
@@ -2,12 +2,13 @@ import mne
 import os
 
 
-subject_id = 102
+subject_id = 104
 raws = []
-data_path = f"/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPRIME/raw/sub-{subject_id}/eeg/"
+data_path = f"/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPRIME/sourcedata/raw/sub-{subject_id}/eeg/"
 raw_filenames = sorted([x for x in os.listdir(data_path) if ".vhdr" in x])
 for filename in raw_filenames:
-	raws.append(mne.io.read_raw_brainvision(data_path + filename, preload=True))
+	if not "flanker_data" in filename:
+		raws.append(mne.io.read_raw_brainvision(data_path + filename, preload=True))
 
 raw = mne.concatenate_raws(raws)
-raw.save(data_path + f"sub-{subject_id}_task-spaceprime_raw.fif")
+raw.save(data_path + f"sub-{subject_id}_task-spaceprime_raw.fif", overwrite=True)
