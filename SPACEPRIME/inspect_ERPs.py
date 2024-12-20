f8758a375beaba761db62c1938aab49d3fd9f137
Max Schulz <schulz.max5@gmail.com>
2024-12-20 14:44:15 +0100

    Visualize subject 104 with adjusted shading intervals

--- SPACEPRIME/inspect_ERPs.py
+++ SPACEPRIME/inspect_ERPs.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 plt.ion()
 
 
-subject_id = 102
+subject_id = 104
 # load epochs
 epochs = mne.read_epochs(f"/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPRIME/derivatives/epoching/sub-{subject_id}/eeg/sub-{subject_id}_task-spaceprime-epo.fif",
                          preload=True)
@@ -50,34 +50,34 @@ fig, ax = plt.subplots(2, 2)
 # first plot
 ax[0][0].plot(times, contra_target_data[0], color="r")
 ax[0][0].plot(times, ipsi_target_data[0], color="b")
-ax[0][0].axvspan(0.65, 0.85, color='gray', alpha=0.3)  # Shade the area
 ax[0][0].axvspan(0.35, 0.55, color='gray', alpha=0.3)  # Shade the area
+ax[0][0].axvspan(0.05, 0.25, color='gray', alpha=0.3)  # Shade the area
 ax[0][0].legend(["Contra", "Ipsi"])
 ax[0][0].set_title("Target lateral")
 ax[0][0].set_ylabel("Amplitude [µV]")
 ax[0][0].set_xlabel("Time [s]")
 # second plot
 ax[0][1].plot(times, contra_singleton_data[0], color="r")
-ax[0][1].axvspan(0.65, 0.85, color='gray', alpha=0.3)  # Shade the area
 ax[0][1].axvspan(0.35, 0.55, color='gray', alpha=0.3)  # Shade the area
+ax[0][1].axvspan(0.05, 0.25, color='gray', alpha=0.3)  # Shade the area
 ax[0][1].plot(times, ipsi_singleton_data[0], color="b")
 ax[0][1].set_title("Singleton lateral")
 ax[0][1].set_ylabel("Amplitude [µV]")
 ax[0][1].set_xlabel("Time [s]")
 # third plot
 ax[1][0].plot(times, result_target[0])
-ax[1][0].axvspan(0.65, 0.85, color='gray', alpha=0.3)  # Shade the area
 ax[1][0].axvspan(0.35, 0.55, color='gray', alpha=0.3)  # Shade the area
+ax[1][0].axvspan(0.05, 0.25, color='gray', alpha=0.3)  # Shade the area
 # fourth plot
 ax[1][1].plot(times, result_singleton[0])
-ax[1][1].axvspan(0.65, 0.85, color='gray', alpha=0.3)  # Shade the area
 ax[1][1].axvspan(0.35, 0.55, color='gray', alpha=0.3)  # Shade the area
+ax[1][1].axvspan(0.05, 0.25, color='gray', alpha=0.3)  # Shade the area
 plt.tight_layout()
 # compute power density spectrum for evoked response and look for frequency tagging
 # first, create epochs objects from numpy arrays computed above for ipsi and contra targets
-ipsi_target_epochs = mne.EpochsArray(data=ipsi_target_epochs_data.reshape(127, 1, 551),
+ipsi_target_epochs = mne.EpochsArray(data=ipsi_target_epochs_data.reshape(len(left_target_epochs), 1, 501),
                                      info=mne.create_info(ch_names=["Ipsi Target"], sfreq=250, ch_types="eeg"))
-contra_target_epochs = mne.EpochsArray(data=contra_target_epochs_data.reshape(127, 1, 551),
+contra_target_epochs = mne.EpochsArray(data=contra_target_epochs_data.reshape(len(left_target_epochs), 1, 501),
                                      info=mne.create_info(ch_names=["Contra Target"], sfreq=250, ch_types="eeg"))
 # now, compute the power spectra for both ipsi and contra targets
 target_diff = mne.EpochsArray(data=(contra_target_epochs.get_data() - ipsi_target_epochs.get_data()),
