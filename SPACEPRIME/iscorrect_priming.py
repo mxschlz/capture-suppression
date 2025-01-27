4e757076ffbf9eb4f86a6167f2ee68977956afd4
Max Schulz <max.schulz10@web.de>
2025-01-27 13:33:15 +0100

    WIP

--- SPACEPRIME/iscorrect_priming.py
+++ SPACEPRIME/iscorrect_priming.py
@@ -1,21 +1,18 @@
-#import matplotlib
-#matplotlib.use("Qt5Agg")
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from color_palette import get_subpalette
+from SPACEPRIME.plotting import plot_individual_lines
+import glob
 plt.ion()
 
-# insert color palette
-sns.set_palette(list(get_subpalette([14, 84, 44]).values()))
 
 # load up dataframe
-df = pd.read_csv("/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPRIME/derivatives/preprocessing/sub-102/beh/sub-102_clean.csv", index_col=0)
+df = pd.read_csv(glob.glob("/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPRIME/derivatives/preprocessing/sub-103/beh/sub-103_clean*.csv")[0])
 # some cleaning
-df = df[(df['event_type'] == 'mouse_click') & (df["phase"] != 2)]
+#df = df[(df['event_type'] == 'mouse_click') & (df["phase"] != 2)]
 df = df[df["SingletonPresent"] == 1]
 
-barplot = sns.barplot(data=df, x="Priming", y="iscorrect", errorbar=None)
+barplot = sns.barplot(data=df, x="Priming", y="select_target", errorbar=None)
 df_mean = df.groupby(['subject_id', 'Priming']).mean(numeric_only=True).reset_index()
 bar_positions = [patch.get_x() + patch.get_width() / 2 for patch in barplot.patches]
 # Plot individual subject data as lines
