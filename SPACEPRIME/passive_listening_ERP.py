689512a02cba4782fac0f96abb0c0b290d5e09e4
Max Schulz <max.schulz10@web.de>
2025-02-05 12:20:49 +0100

    Get some nice ERPs Sanity check to look out for Pd

--- SPACEPRIME/passive_listening_ERP.py
+++ SPACEPRIME/passive_listening_ERP.py
@@ -2,6 +2,7 @@ import mne
 import matplotlib.pyplot as plt
 import os
 import glob
+import numpy as np
 plt.ion()
 
 
@@ -10,5 +11,84 @@ data_root = "/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPR
 # get all the subject ids
 subjects = os.listdir(data_root)
 # load epochs
-epochs = mne.concatenate_epochs([mne.read_epochs(glob.glob(f"/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPRIME/derivatives/epoching/{subject}/eeg/{subject}_task-passive-epo.fif")[0]) for subject in subjects if int(subject.split("-")[1]) in [106, 107]])
+epochs = mne.concatenate_epochs([mne.read_epochs(glob.glob(f"/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPRIME/derivatives/epoching/{subject}/eeg/{subject}_task-passive-epo.fif")[0]) for subject in subjects if int(subject.split("-")[1]) in [105, 107]])
+# get all conditions
+all_conds = list(epochs.event_id.keys())
+# get target, distractor and control epochs
+target_epochs = epochs[[x for x in all_conds if "target" in x]]
+distractor_epochs = epochs[[x for x in all_conds if "distractor" in x]]
+control_epochs = epochs[[x for x in all_conds if "control" in x]]
+# plot ERP
+mne.viz.plot_compare_evokeds([target_epochs.average(), distractor_epochs.average(), control_epochs.average()], picks="FCz")
 
+# plot diff waves
+all_conds = list(epochs.event_id.keys())
+# Separate epochs based on distractor location
+left_singleton_epochs = epochs[[x for x in all_conds if "distractor-location-1" in x]]
+right_singleton_epochs = epochs[[x for x in all_conds if "distractor-location-3" in x]]
+mne.epochs.equalize_epoch_counts([left_singleton_epochs, right_singleton_epochs], method="random")
+# get the contralateral evoked response and average
+contra_singleton_data = np.mean([left_singleton_epochs.copy().average(picks=["C4"]).get_data(),
+                                    right_singleton_epochs.copy().average(picks=["C3"]).get_data()], axis=0)
+# get the ipsilateral evoked response and average
+ipsi_singleton_data = np.mean([left_singleton_epochs.copy().average(picks=["C3"]).get_data(),
+                                  right_singleton_epochs.copy().average(picks=["C4"]).get_data()], axis=0)
+# now, do the same for the lateral targets
+# Separate epochs based on target location
+left_target_epochs = epochs[[x for x in all_conds if "target-location-1" in x]]
+right_target_epochs = epochs[[x for x in all_conds if "target-location-3" in x]]
+mne.epochs.equalize_epoch_counts([left_target_epochs, right_target_epochs], method="random")
+# get the contralateral evoked response and average
+contra_target_data = np.mean([left_target_epochs.copy().average(picks=["C4"]).get_data(),
+                                 right_target_epochs.copy().average(picks=["C3"]).get_data()], axis=0)
+# get the ipsilateral evoked response and average
+ipsi_target_data = np.mean([left_target_epochs.copy().average(picks=["C3"]).get_data(),
+                               right_target_epochs.copy().average(picks=["C4"]).get_data()], axis=0)
+# get the trial-wise data for targets
+contra_target_epochs_data = np.mean(np.concatenate([left_target_epochs.copy().get_data(picks="C4"),
+                                 right_target_epochs.copy().get_data(picks="C3")], axis=1), axis=1)
+ipsi_target_epochs_data = np.mean(np.concatenate([left_target_epochs.copy().get_data(picks="C3"),
+                               right_target_epochs.copy().get_data(picks="C4")], axis=1), axis=1)
+# get the trial-wise data for singletons
+contra_singleton_epochs_data = np.mean(np.concatenate([left_singleton_epochs.copy().get_data(picks="C4"),
+                                 right_singleton_epochs.copy().get_data(picks="C3")], axis=1), axis=1)
+ipsi_singleton_epochs_data = np.mean(np.concatenate([left_singleton_epochs.copy().get_data(picks="C3"),
+                               right_singleton_epochs.copy().get_data(picks="C4")], axis=1), axis=1)
+from scipy.stats import ttest_ind
+result_target = ttest_ind(contra_target_epochs_data, ipsi_target_epochs_data, axis=0)
+result_singleton = ttest_ind(contra_singleton_epochs_data, ipsi_singleton_epochs_data, axis=0)
+# plot the data
+times = epochs.average().times
+fig, ax = plt.subplots(2, 2)
+# first plot
+ax[0][0].plot(times, contra_target_data[0], color="r")
+ax[0][0].plot(times, ipsi_target_data[0], color="b")
+ax[0][0].plot(times, (contra_target_data-ipsi_target_data)[0], color="g")
+ax[0][0].axvspan(0.25, 0.50, color='gray', alpha=0.3)  # Shade the area
+ax[0][0].axvspan(0.05, 0.15, color='gray', alpha=0.3)  # Shade the area
+ax[0][0].hlines(y=0, xmin=times[0], xmax=times[-1])
+ax[0][0].legend(["Contra", "Ipsi", "Contra-Ipsi"])
+ax[0][0].set_title("Target lateral")
+ax[0][0].set_ylabel("Amplitude [µV]")
+ax[0][0].set_xlabel("Time [s]")
+# second plot
+ax[0][1].plot(times, contra_singleton_data[0], color="r")
+ax[0][1].plot(times, ipsi_singleton_data[0], color="b")
+ax[0][1].plot(times, (contra_singleton_data-ipsi_singleton_data)[0], color="g")
+ax[0][1].axvspan(0.25, 0.50, color='gray', alpha=0.3)  # Shade the area
+ax[0][1].axvspan(0.05, 0.15, color='gray', alpha=0.3)  # Shade the area
+ax[0][1].hlines(y=0, xmin=times[0], xmax=times[-1])
+ax[0][1].set_title("Singleton lateral")
+ax[0][1].set_ylabel("Amplitude [µV]")
+ax[0][1].set_xlabel("Time [s]")
+# third plot
+ax[1][0].plot(times, result_target[0])
+ax[1][0].axvspan(0.25, 0.50, color='gray', alpha=0.3)  # Shade the area
+ax[1][0].axvspan(0.05, 0.15, color='gray', alpha=0.3)  # Shade the area
+ax[1][0].hlines(y=0, xmin=times[0], xmax=times[-1])
+# fourth plot
+ax[1][1].plot(times, result_singleton[0])
+ax[1][1].axvspan(0.25, 0.50, color='gray', alpha=0.3)  # Shade the area
+ax[1][1].axvspan(0.05, 0.15, color='gray', alpha=0.3)  # Shade the area
+ax[1][1].hlines(y=0, xmin=times[0], xmax=times[-1])
+plt.tight_layout()
