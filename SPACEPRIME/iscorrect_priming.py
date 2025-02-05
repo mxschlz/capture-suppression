247c8378f3530ea9cd556dddd5a60846d5724f44
Max Schulz <max.schulz10@web.de>
2025-02-05 12:20:26 +0100

    Update

--- SPACEPRIME/iscorrect_priming.py
+++ SPACEPRIME/iscorrect_priming.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from SPACEPRIME.plotting import plot_individual_lines
 import glob
 import os
-plt.ion()
 
 
 # define data root dir
@@ -12,11 +11,11 @@ data_root = "/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPR
 # get all the subject ids
 subjects = os.listdir(data_root)
 # load data from children
-df = pd.concat([pd.read_csv(glob.glob(f"/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPRIME/derivatives/preprocessing/{subject}/beh/{subject}_clean*.csv")[0]) for subject in subjects if int(subject.split("-")[1]) in [103, 105, 106]])
+df = pd.concat([pd.read_csv(glob.glob(f"/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPRIME/derivatives/preprocessing/{subject}/beh/{subject}_clean*.csv")[0]) for subject in subjects if int(subject.split("-")[1]) in [103, 104, 105, 106, 107]])
 df = df[df["SingletonPresent"] == 1]
 # plot
 barplot = sns.barplot(data=df, x="Priming", y="select_target", errorbar=("se", 1))
 plot_individual_lines(ax=barplot, data=df, y_col="select_target")
 plt.xlabel("Priming")
 plt.ylabel("Proportion correct")
-barplot.set_xticklabels([-1, 0, 1])
+barplot.set_xticklabels(["Negative", "No", "Positive"])
