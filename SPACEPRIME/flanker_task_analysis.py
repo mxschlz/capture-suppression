dfd088c899aea763a6a1a5e6a4b7586828c5dc1e
Max <schulz.max5@gmail.com>
2025-02-11 15:42:34 +0100

    Include stats

--- SPACEPRIME/flanker_task_analysis.py
+++ SPACEPRIME/flanker_task_analysis.py
@@ -11,6 +11,7 @@ from SPACEPRIME import get_data_path
 from statsmodels.stats.anova import AnovaRM
 from statsmodels.stats.multitest import multipletests
 from mne.stats import permutation_cluster_test
+from scipy.stats import ttest_ind
 import matplotlib.pyplot as plt
 
 
@@ -55,7 +56,7 @@ print("Reject null hypothesis:", reject)
 
 # get all the congruent and incongruent epochs
 epochs = mne.concatenate_epochs([mne.read_epochs(glob.glob(f"{get_data_path()}derivatives/epoching/{subject}/eeg/{subject}_task-flanker-epo.fif")[0]) for subject in subjects if int(subject.split("-")[1]) in [105, 107]])
-epochs.average().plot("Oz")
+# epochs.average().plot("Oz")
 # compute time frequency bins
 # some params
 freqs = np.arange(1, 31, 1)  # 1 to 30 Hz
@@ -70,35 +71,67 @@ power.average().plot_topo(baseline=(None, 0), mode="logratio")
 # get congruent and incongruent stimulus groups
 congruent_cond = ["congruent_left", "congruent_right"]
 incongruent_cond = ["incongruent_left", "incongruent_right"]
-congruent_power = power[congruent_cond]
-incongruent_power = power[incongruent_cond]
-
-congruent_power_avrg = congruent_power.average().apply_baseline((None, 0), mode="logratio")
-incongruent_power_avrg = incongruent_power.average().apply_baseline((None, 0), mode="logratio")
+# divide epochs into congruent and incongruent
+congruent_epochs = epochs[congruent_cond]
+incongruent_epochs = epochs[incongruent_cond]
+# equalize epoch count
+mne.epochs.equalize_epoch_counts([congruent_epochs, incongruent_epochs], method="random")
+#epochs.apply_baseline(baseline=(None, 0))
+# compute TFR from respective conditions
+congruent_power = congruent_epochs.compute_tfr(method=method, freqs=freqs, n_cycles=n_cycles, decim=decim, n_jobs=-1,
+                                               return_itc=False, average=False)
+incongruent_power = incongruent_epochs.compute_tfr(method=method, freqs=freqs, n_cycles=n_cycles, decim=decim, n_jobs=-1,
+                                               return_itc=False, average=False)
+congruent_power_avrg = congruent_power.average()
+incongruent_power_avrg = incongruent_power.average()
+# apply baseline to the data
+congruent_power_avrg.apply_baseline(mode="percent", baseline=(None, 0))
+incongruent_power_avrg.apply_baseline(mode="percent", baseline=(None, 0))
 diff = incongruent_power_avrg - congruent_power_avrg
+diff.plot_topo()
+# pick channel of interest
+picks = mne.pick_channels(epochs.ch_names, include=["CPz"])
+# extract the alpha band activity and average over the frequency dimension
+incongruent_alpha_data_avrg = incongruent_power_avrg.get_data(picks=picks, fmin=8, fmax=12).mean(axis=1)
+congruent_alpha_data_avrg = congruent_power_avrg.get_data(picks=picks, fmin=8, fmax=12).mean(axis=1)
+# do independent t test
+incongruent_alpha_data = incongruent_power.get_data(picks=picks, fmin=8, fmax=12).mean(axis=2)
+congruent_alpha_data = congruent_power.get_data(picks=picks, fmin=8, fmax=12).mean(axis=2)
+ttest = ttest_ind(incongruent_alpha_data, congruent_alpha_data, axis=0)
+# plot the stuff
+times = power.times
+fig, ax = plt.subplots(2, 1)
+ax[0].plot(times, incongruent_alpha_data_avrg[0], color="red", label="incongruent alpha")
+ax[0].plot(times, congruent_alpha_data_avrg[0], color="blue", label="congruent alpha")
+ax[1].plot(times, ttest[0][0], color="lightgrey", label="t values")
+ax[0].legend()
+ax[1].legend()
 # do cluster-based permutation test
 n_permutations = 10000
 n_jobs = -1
 pval = 0.05
 threshold = dict(start=0, step=0.2)  # the smaller the step and the closer the start to 0, the better the approximation
 adjacency, _ = mne.channels.find_ch_adjacency(
-    power.info, "eeg")
+    diff.info, "eeg")
 # plt.matshow(adjacency.toarray())  # take a look at the matrix
 
 # mne.viz.plot_ch_adjacency(epochs.info, adjacency, epochs.info["ch_names"])
-X = [congruent_power_avrg.get_data(), incongruent_power_avrg.get_data()]
-t_obs, clusters, cluster_pv, h0 = mne.stats.permutation_cluster_test(
-    X, threshold=threshold,n_permutations=n_permutations, n_jobs=n_jobs, out_type="mask")
-
+# get the data points for respective TFR conditions
+# transpose the data because the permutation test expects the channels to be last
+incongruent_power_data = incongruent_power.get_data(picks=picks).transpose(0, 2, 3, 1)
+congruent_power_data = congruent_power.get_data(picks=picks).transpose(0, 2, 3, 1)
+X = [incongruent_power_data, congruent_power_data]
+t_obs, clusters, cluster_pv, h0 = permutation_cluster_test(X, threshold=threshold,n_permutations=n_permutations,
+                                                           n_jobs=n_jobs, out_type="mask")
+# get rid of channel dimension
+t_obs = t_obs.mean(axis=2)
 # plot the results
 times = epochs.times
 fig, (ax, ax2) = plt.subplots(2, 1, figsize=(6, 4), layout="constrained")
 
 # Compute the difference in evoked to determine which was greater since
 # we used a 1-way ANOVA which tested for a difference in population means
-evoked_power_1 = incongruent_power_avrg.get_data().mean(axis=0)
-evoked_power_2 = congruent_power.get_data().mean(axis=0)
-evoked_power_contrast = evoked_power_1 - evoked_power_2
+evoked_power_contrast = diff.get_data(picks=picks)
 signs = np.sign(evoked_power_contrast)
 
 # Create new stats image with only significant clusters
@@ -130,9 +163,9 @@ ax.set_ylabel("Frequency (Hz)")
 ax.set_title(f"Induced power")
 
 # plot evoked
-evoked_condition_1 = epochs_condition_1.average()
-evoked_condition_2 = epochs_condition_2.average()
+evoked_condition_1 = incongruent_epochs.average()
+evoked_condition_2 = congruent_epochs.average()
 evoked_contrast = mne.combine_evoked(
     [evoked_condition_1, evoked_condition_2], weights=[1, -1]
 )
-evoked_contrast.plot(axes=ax2, time_unit="s")
+evoked_contrast.plot(axes=ax2, time_unit="s", picks=picks)
