09f366fb6b337e7d03e8e3c124fe7f18325f3a51
Max Schulz <max.schulz10@web.de>
2025-02-17 14:37:34 +0100

    Deleted obsolete code

--- SPACEPRIME/inspect_N1_Pd.py
+++ SPACEPRIME/inspect_N1_Pd.py
@@ -13,7 +13,7 @@ plt.ion()
 data_root = SPACEPRIME.get_data_path()+"derivatives/preprocessing/"
 # get all the subject ids
 subjects = os.listdir(data_root)
-sub_ids = [103, 104, 105, 106, 108, 110]
+sub_ids = [103, 104, 105, 106, 108, 110, 112, 114]
 # load epochs
 epochs = mne.concatenate_epochs([mne.read_epochs(glob.glob(f"{SPACEPRIME.get_data_path()}derivatives/epoching/{subject}/eeg/{subject}_task-spaceprime-epo.fif")[0]) for subject in subjects if int(subject.split("-")[1]) in sub_ids])
 #epochs = epochs["select_target==True"]
@@ -23,24 +23,12 @@ all_conds = list(epochs.event_id.keys())
 left_singleton_epochs = epochs[[x for x in all_conds if "Target-2-Singleton-1" in x]]
 right_singleton_epochs = epochs[[x for x in all_conds if "Target-2-Singleton-3" in x]]
 mne.epochs.equalize_epoch_counts([left_singleton_epochs, right_singleton_epochs], method="random")
-# get the contralateral evoked response and average
-contra_singleton_data = np.mean([left_singleton_epochs.copy().average(picks=["C4"]).get_data(),
-                                    right_singleton_epochs.copy().average(picks=["C3"]).get_data()], axis=0)
-# get the ipsilateral evoked response and average
-ipsi_singleton_data = np.mean([left_singleton_epochs.copy().average(picks=["C3"]).get_data(),
-                                  right_singleton_epochs.copy().average(picks=["C4"]).get_data()], axis=0)
 
 # now, do the same for the lateral targets
 # Separate epochs based on target location
 left_target_epochs = epochs[[x for x in all_conds if "Target-1-Singleton-2" in x]]
 right_target_epochs = epochs[[x for x in all_conds if "Target-3-Singleton-2" in x]]
 mne.epochs.equalize_epoch_counts([left_target_epochs, right_target_epochs], method="random")
-# get the contralateral evoked response and average
-contra_target_data = np.mean([left_target_epochs.copy().average(picks=["C4"]).get_data(),
-                                 right_target_epochs.copy().average(picks=["C3"]).get_data()], axis=0)
-# get the ipsilateral evoked response and average
-ipsi_target_data = np.mean([left_target_epochs.copy().average(picks=["C3"]).get_data(),
-                               right_target_epochs.copy().average(picks=["C4"]).get_data()], axis=0)
 
 # get the trial-wise data for targets
 contra_target_epochs_data = np.mean(np.concatenate([left_target_epochs.copy().get_data(picks="C4"),
@@ -54,18 +42,19 @@ ipsi_singleton_epochs_data = np.mean(np.concatenate([left_singleton_epochs.copy(
                                right_singleton_epochs.copy().get_data(picks="C4")], axis=1), axis=1)
 
 # get difference waves
-diff_wave_target = contra_target_data - ipsi_target_data
-diff_wave_distractor = contra_singleton_data - ipsi_singleton_data
+diff_wave_target = contra_target_epochs_data.mean(axis=0) - ipsi_target_epochs_data.mean(axis=0)
+diff_wave_distractor = contra_singleton_epochs_data.mean(axis=0) - ipsi_singleton_epochs_data.mean(axis=0)
 # run ttests
 result_target = ttest_ind(contra_target_epochs_data, ipsi_target_epochs_data, axis=0)
 result_singleton = ttest_ind(contra_singleton_epochs_data, ipsi_singleton_epochs_data, axis=0)
 # plot the data
 times = epochs.average().times
-fig, ax = plt.subplots(2, 2)
+fig, ax = plt.subplots(2, 2, sharey=False)
+ax[1][1].sharey(ax[1][0])
 # first plot
-ax[0][0].plot(times, contra_target_data[0], color="r")
-ax[0][0].plot(times, ipsi_target_data[0], color="b")
-ax[0][0].plot(times, diff_wave_target[0], color="g")
+ax[0][0].plot(times, contra_target_epochs_data.mean(axis=0), color="r")
+ax[0][0].plot(times, ipsi_target_epochs_data.mean(axis=0), color="b")
+ax[0][0].plot(times, diff_wave_target, color="g")
 ax[0][0].axvspan(0.2, 0.30, color='gray', alpha=0.3)  # Shade the area
 ax[0][0].axvspan(0.05, 0.15, color='gray', alpha=0.3)  # Shade the area
 ax[0][0].hlines(y=0, xmin=times[0], xmax=times[-1])
@@ -74,9 +63,9 @@ ax[0][0].set_title("Target lateral")
 ax[0][0].set_ylabel("Amplitude [µV]")
 ax[0][0].set_xlabel("Time [s]")
 # second plot
-ax[0][1].plot(times, contra_singleton_data[0], color="r")
-ax[0][1].plot(times, ipsi_singleton_data[0], color="b")
-ax[0][1].plot(times, diff_wave_distractor[0], color="g")
+ax[0][1].plot(times, contra_singleton_epochs_data.mean(axis=0), color="r")
+ax[0][1].plot(times, ipsi_singleton_epochs_data.mean(axis=0), color="b")
+ax[0][1].plot(times, diff_wave_distractor, color="g")
 ax[0][1].axvspan(0.25, 0.50, color='gray', alpha=0.3)  # Shade the area
 ax[0][1].axvspan(0.05, 0.15, color='gray', alpha=0.3)  # Shade the area
 ax[0][1].hlines(y=0, xmin=times[0], xmax=times[-1])
@@ -95,6 +84,21 @@ ax[1][1].axvspan(0.05, 0.15, color='gray', alpha=0.3)  # Shade the area
 ax[1][1].hlines(y=0, xmin=times[0], xmax=times[-1])
 plt.tight_layout()
 
+# extract all channel data to plot topographies
+# get the trial-wise data for targets
+contra_target_epochs_data_all_chs = np.mean(np.concatenate([left_target_epochs.copy().get_data(),
+                                                            right_target_epochs.copy().get_data()], axis=0),
+                                            axis=0)
+ipsi_target_epochs_data_all_chs = np.mean(np.concatenate([left_target_epochs.copy().get_data(),
+                                                          right_target_epochs.copy().get_data()], axis=0),
+                                          axis=0)
+# get the trial-wise data for singletons
+contra_singleton_epochs_data_all_chs = np.mean(np.concatenate([left_singleton_epochs.copy().get_data(),
+                                                               right_singleton_epochs.copy().get_data()], axis=0),
+                                               axis=0)
+ipsi_singleton_epochs_data_all_chs = np.mean(np.concatenate([left_singleton_epochs.copy().get_data(),
+                                                             right_singleton_epochs.copy().get_data()], axis=0),
+                                             axis=0)
 
 # number of permutations
 n_permutations = 10000
@@ -104,7 +108,7 @@ pval = 0.05
 threshold = dict(start=0, step=0.2)  # the smaller the step and the closer the start to 0, the better the approximation
 
 # mne.viz.plot_ch_adjacency(epochs.info, adjacency, epochs.info["ch_names"])
-X = [contra_target_epochs_data, ipsi_target_epochs_data]
+X = [contra_singleton_epochs_data, ipsi_singleton_epochs_data]
 t_obs, clusters, cluster_pv, h0 = permutation_cluster_test(X, threshold=threshold, n_permutations=n_permutations,
                                                            n_jobs=n_jobs, out_type="mask", tail=0)
 times = epochs.times
@@ -112,7 +116,7 @@ fig, (ax, ax2) = plt.subplots(2, 1, figsize=(8, 4))
 ax.set_title("Contra minus ipsi")
 ax.plot(
     times,
-    contra_target_epochs_data.mean(axis=0) - ipsi_target_epochs_data.mean(axis=0),
+    contra_singleton_epochs_data.mean(axis=0) - ipsi_singleton_epochs_data.mean(axis=0),
     label="ERP Contrast (Contra minus ipsi)",
 )
 ax.set_ylabel("EEG (µV)")
