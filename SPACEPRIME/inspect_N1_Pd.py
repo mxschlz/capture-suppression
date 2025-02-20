f52af758f5d5f75e9212edaad32fdd4d72d68d41
Max Schulz <max.schulz10@web.de>
2025-02-20 20:17:37 +0100

    Subtract passive listening ERPs (WIP)

--- SPACEPRIME/inspect_N1_Pd.py
+++ SPACEPRIME/inspect_N1_Pd.py
@@ -6,6 +6,8 @@ from SPACEPRIME import get_data_path
 from mne.stats import permutation_cluster_test
 from scipy.stats import ttest_ind
 from SPACEPRIME.subjects import subject_ids
+from SPACEPRIME.plotting import difference_topos
+from SPACEPRIME.passive_listening_ERP import get_passive_listening_ERPs
 plt.ion()
 
 
@@ -13,7 +15,8 @@ settings_path = f"{get_data_path()}settings/"
 montage = mne.channels.read_custom_montage(settings_path + "CACS-64_NO_REF.bvef")
 ch_pos = montage.get_positions()["ch_pos"]
 # load epochs
-epochs = mne.concatenate_epochs([mne.read_epochs(glob.glob(f"{get_data_path()}derivatives/epoching/sub-{subject}/eeg/sub-{subject}_task-spaceprime-epo.fif")[0]) for subject in subject_ids])
+epochs = mne.concatenate_epochs([mne.read_epochs(glob.glob(f"{get_data_path()}derivatives/epoching/sub-{subject}/eeg/sub-{subject}_task-spaceprime-epo.fif")[0]) for subject in subject_ids[2:]])
+epochs.crop(-0.1, 0.6)
 #epochs = epochs["select_target==True"]
 # epochs.apply_baseline()
 all_conds = list(epochs.event_id.keys())
@@ -41,20 +44,24 @@ contra_distractor_epochs_data = np.mean(np.concatenate([left_distractor_epochs.c
 ipsi_distractor_epochs_data = np.mean(np.concatenate([left_distractor_epochs.copy().get_data(picks="C3"),
                                right_distractor_epochs.copy().get_data(picks="C4")], axis=1), axis=1)
 
+contra_distractor_epochs_data_passive, ipsi_distractor_epochs_data_passive, contra_target_epochs_data_passive, ipsi_target_epochs_data_passive, diff_target_passive, diff_distractor_passive = get_passive_listening_ERPs()
+
 # get difference waves
 diff_wave_target = contra_target_epochs_data.mean(axis=0) - ipsi_target_epochs_data.mean(axis=0)
 diff_wave_distractor = contra_distractor_epochs_data.mean(axis=0) - ipsi_distractor_epochs_data.mean(axis=0)
 # run ttests
-result_target = ttest_ind(contra_target_epochs_data, ipsi_target_epochs_data, axis=0)
-result_distractor = ttest_ind(contra_distractor_epochs_data, ipsi_distractor_epochs_data, axis=0)
+result_target = ttest_ind(contra_target_epochs_data-contra_target_epochs_data_passive[0],
+                          ipsi_target_epochs_data-ipsi_target_epochs_data_passive[0], axis=0)
+result_distractor = ttest_ind(contra_distractor_epochs_data-contra_distractor_epochs_data_passive[0],
+                              ipsi_distractor_epochs_data-ipsi_distractor_epochs_data_passive[0], axis=0)
 # plot the data
-times = epochs.average().times
+times = epochs.times
 fig, ax = plt.subplots(2, 2, sharey=False)
 ax[1][1].sharey(ax[1][0])
 # first plot
-ax[0][0].plot(times, contra_target_epochs_data.mean(axis=0), color="r")
-ax[0][0].plot(times, ipsi_target_epochs_data.mean(axis=0), color="b")
-ax[0][0].plot(times, diff_wave_target, color="g")
+ax[0][0].plot(times, contra_target_epochs_data.mean(axis=0)-contra_target_epochs_data_passive.mean(axis=0), color="r")
+ax[0][0].plot(times, ipsi_target_epochs_data.mean(axis=0)-ipsi_target_epochs_data_passive.mean(axis=0), color="b")
+ax[0][0].plot(times, (diff_wave_target-diff_target_passive)[0], color="g")
 ax[0][0].axvspan(0.2, 0.30, color='gray', alpha=0.3)  # Shade the area
 ax[0][0].axvspan(0.05, 0.15, color='gray', alpha=0.3)  # Shade the area
 ax[0][0].hlines(y=0, xmin=times[0], xmax=times[-1])
@@ -63,13 +70,13 @@ ax[0][0].set_title("Target lateral")
 ax[0][0].set_ylabel("Amplitude [µV]")
 ax[0][0].set_xlabel("Time [s]")
 # second plot
-ax[0][1].plot(times, contra_distractor_epochs_data.mean(axis=0), color="r")
-ax[0][1].plot(times, ipsi_distractor_epochs_data.mean(axis=0), color="b")
-ax[0][1].plot(times, diff_wave_distractor, color="g")
+ax[0][1].plot(times, contra_distractor_epochs_data.mean(axis=0)-contra_distractor_epochs_data_passive.mean(axis=0), color="r")
+ax[0][1].plot(times, ipsi_distractor_epochs_data.mean(axis=0)-ipsi_distractor_epochs_data_passive.mean(axis=0), color="b")
+ax[0][1].plot(times, (diff_wave_distractor-diff_distractor_passive)[0], color="g")
 ax[0][1].axvspan(0.25, 0.50, color='gray', alpha=0.3)  # Shade the area
 ax[0][1].axvspan(0.05, 0.15, color='gray', alpha=0.3)  # Shade the area
 ax[0][1].hlines(y=0, xmin=times[0], xmax=times[-1])
-ax[0][1].set_title("distractor lateral")
+ax[0][1].set_title("Distractor lateral")
 ax[0][1].set_ylabel("Amplitude [µV]")
 ax[0][1].set_xlabel("Time [s]")
 # third plot
@@ -144,66 +151,25 @@ ax2.legend((h,), ("cluster p-value < 0.05",))
 ax2.set_xlabel("time (ms)")
 ax2.set_ylabel("statistic value")  # which statistic?
 
-# --- Difference Wave Calculation ---
-diff_waves_target = {}
-diff_waves_distractor = {}
-
-for ch_name in all_chs:
-    if not "z" in ch_name:
-        # Determine hemisphere of current channel
-        try:
-          channel_position = ch_pos[ch_name]
-        except:
-          continue
-        is_left = channel_position[0] < 0  # x coordinate negative = left
-
-        # --- TARGET ---
-        if is_left:
-            contra_target_epochs = epochs[[x for x in all_conds if "Target-3-Singleton-2" in x]]
-            ipsi_target_epochs = epochs[[x for x in all_conds if "Target-1-Singleton-2" in x]]
-        else:  # Right hemisphere
-            contra_target_epochs = epochs[[x for x in all_conds if "Target-1-Singleton-2" in x]]
-            ipsi_target_epochs = epochs[[x for x in all_conds if "Target-3-Singleton-2" in x]]
-
-        mne.epochs.equalize_epoch_counts([contra_target_epochs, ipsi_target_epochs], method="random")
-
-        contra_target_data = contra_target_epochs.get_data(picks=ch_name).mean(axis=0)  # Average trials
-        ipsi_target_data = ipsi_target_epochs.get_data(picks=ch_name).mean(axis=0)
-        diff_waves_target[ch_name] = (contra_target_data - ipsi_target_data)[0]
-
-        # --- DISTRACTOR ---
-        if is_left:
-            contra_distractor_epochs = epochs[[x for x in all_conds if "Target-2-Singleton-3" in x]]
-            ipsi_distractor_epochs = epochs[[x for x in all_conds if "Target-2-Singleton-1" in x]]
-        else:
-            contra_distractor_epochs = epochs[[x for x in all_conds if "Target-2-Singleton-1" in x]]
-            ipsi_distractor_epochs = epochs[[x for x in all_conds if "Target-2-Singleton-3" in x]]
-
-        mne.epochs.equalize_epoch_counts([contra_distractor_epochs, ipsi_distractor_epochs], method="random")
-
-        contra_distractor_data = contra_distractor_epochs.get_data(picks=ch_name).mean(axis=0)
-        ipsi_distractor_data = ipsi_distractor_epochs.get_data(picks=ch_name).mean(axis=0)
-        diff_waves_distractor[ch_name] = (contra_distractor_data - ipsi_distractor_data)[0]
-    else:
-        diff_waves_distractor[ch_name] = np.zeros((len(epochs.times)))
-        diff_waves_target[ch_name] = np.zeros((len(epochs.times)))
+# calculate difference waves
+diff_waves_target, diff_waves_distractor = difference_topos(epochs=epochs, montage=montage)
 
 # --- Topomap Sequence Figure ---
 info = epochs.info
 # 1. Define Time Range and Step
 start_time = 0  # Example: Start 100ms *before* stimulus onset
-end_time = 0.7   # Example: End 500ms after stimulus onset
+end_time = 0.6  # Example: End 500ms after stimulus onset
 time_step = 0.05  # 50ms step
 
 # 2. Create Time Points
 times_to_plot = np.arange(start_time, end_time + time_step, time_step)
 
 # 3. Choose Difference Wave (Target or Distractor)
-#   Uncomment the one you want to plot, and comment out the other.
-# diff_waves = diff_waves_target  # For TARGET difference waves
-diff_waves = diff_waves_distractor  # For DISTRACTOR difference waves
-# plot_title_prefix = "Target" # if diff_waves is diff_waves_target
-plot_title_prefix = "Distractor" # if diff_waves is diff_waves_distractor
+plot_title_prefix = "Target"  # if diff_waves is diff_waves_distractor
+if plot_title_prefix == "Target":
+    diff_waves = diff_waves_target  # For TARGET difference waves
+elif plot_title_prefix == "Distractor":
+    diff_waves = diff_waves_distractor  # For DISTRACTOR difference waves
 
 # 4. Determine Figure Layout (Rows and Columns)
 n_plots = len(times_to_plot)
@@ -233,7 +199,7 @@ for i, time_point in enumerate(times_to_plot):
 
     # Now pass the `cmap` to plot_topomap
     mne.viz.plot_topomap(data_for_plot, info, axes=axes[i], cmap=cmap)
-    axes[i].set_title(f"{time_point*1000:.0f} ms")
+    axes[i].set_title(f"{time_point * 1000:.0f} ms")
 
 # 9. Remove Unused Subplots
 for j in range(i + 1, len(axes)):
