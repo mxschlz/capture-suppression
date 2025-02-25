be4f25d14e90d98e56ccfd74994f1f8886bf74b6
Max Schulz <max.schulz10@web.de>
2025-02-25 08:38:09 +0100

    Subtract or add events

--- SPACEPRIME/rename_events.py
+++ SPACEPRIME/rename_events.py
@@ -1,4 +1,5 @@
 import mne
+import numpy as np
 
 
 def add_to_events(epochs_orig, new_encoding, change_by=1):
@@ -8,8 +9,8 @@ def add_to_events(epochs_orig, new_encoding, change_by=1):
                                      tmin=epochs_orig.tmin)
     if change_by > 0:
         if epochs_orig.event_id:
-            epochs_renamed.event_id = {k: v + 1 for k, v in epochs_orig.event_id.items()}
+            epochs_renamed.event_id = {k: v + np.abs(change_by) for k, v in epochs_orig.event_id.items()}
     elif change_by < 0:
         if epochs_orig.event_id:
-            epochs_renamed.event_id = {k: v - 1 for k, v in epochs_orig.event_id.items()}
+            epochs_renamed.event_id = {k: v - np.abs(change_by) for k, v in epochs_orig.event_id.items()}
     return epochs_renamed
