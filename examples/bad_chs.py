c33e995f5869c5604b6be44fe2ce6d54970ef0de
Max Schulz <max.schulz10@web.de>
2025-03-10 22:07:09 +0100

    Bad chs

---
+++ examples/bad_chs.py
@@ -0,0 +1,16 @@
+bads = {
+    "SP_EEG_P0006": ["AF8"], # 73 trials
+    "SP_EEG_P0023": ["T7"], # 94 trials
+    "SP_EEG_P0033": ["AF8"], # 181 trials
+    "SP_EEG_P0051": ["TP10"], # 91 trials
+    "SP_EEG_P0053": None, # 271 trials
+    "SP_EEG_P0054": ["T7", "T8"], # 276 trials
+    "SP_EEG_P0058": ["AF8"], # 4 trials
+    "SP_EEG_P0061": ["T7", "T8"], # 270 trials
+    "SP_EEG_P0062": None, # 282 trials
+    "SP_EEG_P0065": ["T7"], # 8 trials
+    "SP_EEG_P0066": None, # 224 trials
+    "SP_EEG_P0077": ["CPz"], # 228 trials
+    "SP_EEG_P0085": ["Fp1"],
+    "SP_EEG_P0087": None
+}
