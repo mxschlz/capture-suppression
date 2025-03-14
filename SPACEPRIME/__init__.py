bceabdcc81c0f5c5d052d9ae0b6370e403d5734f
maxschulz <schulz.max5@gmail.com>
2025-03-14 13:56:16 +0100

    Adapt to Rechenknecht

--- SPACEPRIME/__init__.py
+++ SPACEPRIME/__init__.py
@@ -1,11 +1,18 @@
 import os
+import socket
+
 
 def get_data_path():
-  """Returns the correct file path to the EEG data based on the OS."""
-  if os.name == 'nt':  # Windows
-    data_path = 'G:\\Meine Ablage\\PhD\\data\\SPACEPRIME\\'
-  elif os.name == 'posix':  # Linux
-    data_path = '/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPRIME/'
-  else:
-    raise OSError("Unsupported operating system.")
-  return data_path
+    if os.name == 'nt':
+        data_path = 'G:\\Meine Ablage\\PhD\\data\\SPACEPRIME\\'
+    elif os.name == 'posix':
+        hostname = socket.gethostname()
+        if "rechenknecht" in hostname:
+            data_path = '/home/maxschulz/IPSY1-Storage/Projects/ac/Experiments/running_studies/SPACEPRIME/'
+        elif 'MaxPC' in hostname:
+            data_path = '/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPRIME/'
+        else:
+            raise OSError(f"Unknown Linux machine: {hostname}")
+    else:
+        raise OSError("Unsupported operating system.")
+    return data_path
