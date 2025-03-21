405142bb302f3f72e978228753a2b0c151c626a3
Max Schulz <max.schulz10@web.de>
2025-03-21 09:18:53 +0100

    Within-subject t test

--- SPACEPRIME/iscorrect_priming.py
+++ SPACEPRIME/iscorrect_priming.py
@@ -4,22 +4,23 @@ import matplotlib.pyplot as plt
 from SPACEPRIME.plotting import plot_individual_lines
 import glob
 from SPACEPRIME import get_data_path
-from scipy.stats import ttest_ind
+from scipy.stats import ttest_rel
 from stats import cronbach_alpha
 from SPACEPRIME.subjects import subject_ids
 
 
 df = pd.concat([pd.read_csv(glob.glob(f"{get_data_path()}derivatives/preprocessing/sub-{subject}/beh/sub-{subject}_clean*.csv")[0]) for subject in subject_ids])
 #df = df[df["SingletonPresent"] == 0]
+df_mean = df.groupby(["subject_id", "Priming"])["select_target"].mean().reset_index()
 # plot
-barplot = sns.barplot(data=df, x="Priming", y="select_target", errorbar=("se", 1))
-plot_individual_lines(ax=barplot, data=df, y_col="select_target")
+barplot = sns.barplot(data=df_mean, x="Priming", y="select_target", errorbar=("se", 1))
+plot_individual_lines(ax=barplot, data=df_mean, y_col="select_target")
 plt.xlabel("Priming")
 plt.ylabel("Proportion correct")
 barplot.set_xticklabels(["Negative", "No", "Positive"])
 # ttest
-t, pval = ttest_ind(df.query("Priming==-1")["select_target"].astype(int),
-                    df.query("Priming==0")["select_target"].astype(int), nan_policy="omit")
+t, pval = ttest_rel(df_mean.query("Priming==0")["rt"].astype(float),
+                    df_mean.query("Priming==-1")["rt"].astype(float), nan_policy="omit")
 
 df_pivot = df.groupby(["subject_id", "Priming"])["select_target"].mean().reset_index().pivot(index="subject_id",
                                                                                              columns="Priming",
