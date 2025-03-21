7da72d8afe5e23811c024874e8280cc62f11ceaa
Max <schulz.max5@gmail.com>
2025-03-21 17:29:28 +0100

    Removed redundant topoplot code Do not equalize epoch count

--- SPACEPRIME/N2ac_Pd.py
+++ SPACEPRIME/N2ac_Pd.py
@@ -5,12 +5,11 @@ import glob
 from scipy.signal import savgol_filter
 from SPACEPRIME import get_data_path
 from SPACEPRIME.subjects import subject_ids
-from SPACEPRIME.plotting import difference_topos
 plt.ion()
 
 
-epochs = mne.concatenate_epochs([mne.read_epochs(glob.glob(f"{get_data_path()}derivatives/epoching/sub-{subject}/eeg/sub-{subject}_task-spaceprime-epo.fif")[0], preload=False) for subject in subject_ids[:-5]])
-epochs.apply_baseline((-0.3, 0))
+epochs = mne.concatenate_epochs([mne.read_epochs(glob.glob(f"{get_data_path()}derivatives/epoching/sub-{subject}/eeg/sub-{subject}_task-spaceprime-epo.fif")[0], preload=False) for subject in subject_ids])
+# epochs.apply_baseline((-0.3, 0))
 # epochs = epochs["select_target==True"]
 epochs.crop(0, 0.7)  # crop for better comparability with Mandal et al. (2024)
 # epochs = epochs["Priming==0"]
@@ -25,7 +24,7 @@ montage = mne.channels.read_custom_montage(settings_path + "CACS-64_NO_REF.bvef"
 # Separate epochs based on distractor location
 left_singleton_epochs = epochs[[x for x in all_conds if "Target-2-Singleton-1" in x]]
 right_singleton_epochs = epochs[[x for x in all_conds if "Target-2-Singleton-3" in x]]
-mne.epochs.equalize_epoch_counts([left_singleton_epochs, right_singleton_epochs], method="random")
+#mne.epochs.equalize_epoch_counts([left_singleton_epochs, right_singleton_epochs], method="random")
 # get the contralateral evoked response and average
 contra_singleton_data = np.mean([left_singleton_epochs.copy().average(picks="FC6").get_data(),
                                     right_singleton_epochs.copy().average(picks="FC5").get_data()], axis=0)
@@ -37,7 +36,7 @@ ipsi_singleton_data = np.mean([left_singleton_epochs.copy().average(picks="FC5")
 # Separate epochs based on target location
 left_target_epochs = epochs[[x for x in all_conds if "Target-1-Singleton-2" in x]]
 right_target_epochs = epochs[[x for x in all_conds if "Target-3-Singleton-2" in x]]
-mne.epochs.equalize_epoch_counts([left_target_epochs, right_target_epochs], method="random")
+#mne.epochs.equalize_epoch_counts([left_target_epochs, right_target_epochs], method="random")
 # get the contralateral evoked response and average
 contra_target_data = np.mean([left_target_epochs.copy().average(picks="FC6").get_data(),
                                  right_target_epochs.copy().average(picks="FC5").get_data()], axis=0)
@@ -48,7 +47,7 @@ ipsi_target_data = np.mean([left_target_epochs.copy().average(picks="FC5").get_d
 # we can also isolate target effects without a distractor presence
 left_target_epochs_distractor_absent = epochs[[x for x in all_conds if "Target-1-Singleton-0" in x]]
 right_target_epochs_distractor_absent = epochs[[x for x in all_conds if "Target-3-Singleton-0" in x]]
-mne.epochs.equalize_epoch_counts([left_target_epochs_distractor_absent, right_target_epochs_distractor_absent], method="random")
+#mne.epochs.equalize_epoch_counts([left_target_epochs_distractor_absent, right_target_epochs_distractor_absent], method="random")
 # get the contralateral evoked response and average
 contra_target_distractor_absent_data = np.mean([left_target_epochs_distractor_absent.copy().average(picks="FC6").get_data(),
                                  right_target_epochs_distractor_absent.copy().average(picks="FC5").get_data()], axis=0)
@@ -65,6 +64,7 @@ diff_wave_distractor = contra_singleton_data - ipsi_singleton_data
 window_length = 51
 poly_order = 3
 times = epochs.average().times
+plt.figure()
 plt.plot(times, savgol_filter(diff_wave_distractor[0]*10e5, window_length=window_length, polyorder=poly_order), color="darkorange")
 plt.plot(times, savgol_filter(diff_wave_target_distractor_present[0]*10e5, window_length=window_length, polyorder=poly_order), color="blue")
 plt.plot(times, savgol_filter(diff_wave_target_distractor_absent[0]*10e5, window_length=window_length, polyorder=poly_order), color="green")
@@ -73,67 +73,3 @@ plt.legend(["Distractor lateral", "Target lateral (Distractor present)", "Target
 plt.title("Difference Contra - Ipsi")
 plt.ylabel("Amplitude [µV]")
 plt.xlabel("Time [s]")
-
-# compute difference waves for each channel
-diff_waves_target, diff_waves_distractor = difference_topos(epochs=epochs, montage=montage)
-
-# --- Topomap Sequence Figure ---
-info = epochs.info
-# 1. Define Time Range and Step
-start_time = 0  # Example: Start 100ms *before* stimulus onset
-end_time = 0.6  # Example: End 500ms after stimulus onset
-time_step = 0.05  # 50ms step
-
-# 2. Create Time Points
-times_to_plot = np.arange(start_time, end_time + time_step, time_step)
-
-# 3. Choose Difference Wave (Target or Distractor)
-plot_title_prefix = "Target"  # if diff_waves is diff_waves_distractor
-if plot_title_prefix == "Target":
-    diff_waves = diff_waves_target  # For TARGET difference waves
-elif plot_title_prefix == "Distractor":
-    diff_waves = diff_waves_distractor  # For DISTRACTOR difference waves
-
-# 4. Determine Figure Layout (Rows and Columns)
-n_plots = len(times_to_plot)
-n_cols = int(np.ceil(np.sqrt(n_plots)))
-n_rows = int(np.ceil(n_plots / n_cols))
-
-# 5. Create Figure and Subplots
-fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 8))
-axes = axes.flatten()
-
-# 6.  Find Consistent vmin and vmax
-all_values = []
-for ch in diff_waves:
-    for time_idx in [epochs.time_as_index(t)[0] for t in times_to_plot]:
-        all_values.append(diff_waves[ch][time_idx])
-vmin = np.min(all_values)
-vmax = np.max(all_values)
-print(f'vmin = {vmin}, vmax = {vmax}')
-
-# 7. Choose the colormap *explicitly*
-cmap = 'RdBu_r'  # Or any other colormap you prefer
-
-# 8. Loop and Plot
-for i, time_point in enumerate(times_to_plot):
-    time_idx = epochs.time_as_index(time_point)[0]
-    data_for_plot = np.array([diff_waves[ch][time_idx] for ch in all_chs if ch in diff_waves])
-
-    # Now pass the `cmap` to plot_topomap
-    mne.viz.plot_topomap(data_for_plot, info, axes=axes[i], cmap=cmap)
-    axes[i].set_title(f"{time_point * 1000:.0f} ms")
-
-# 9. Remove Unused Subplots
-for j in range(i + 1, len(axes)):
-    fig.delaxes(axes[j])
-
-# 10. Create ScalarMappable with the correct colormap
-sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
-
-# 11. Add Colorbar and Title (using the ScalarMappable)
-fig.subplots_adjust(right=0.8)
-cbar_ax = fig.add_axes([0.85, 0.15, 0.02, 0.7])
-plt.colorbar(sm, cax=cbar_ax)  # Use the ScalarMappable here
-
-fig.suptitle(f"{plot_title_prefix} Difference Wave Topomaps (50ms Steps)", fontsize=16)
