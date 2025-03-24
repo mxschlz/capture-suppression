f82542397bbf79ba600899ec6ae9fb007171c477
Max <schulz.max5@gmail.com>
2025-03-24 16:19:11 +0100

    Remove outliers

--- SPACEPRIME/iscorrect_priming.py
+++ SPACEPRIME/iscorrect_priming.py
@@ -6,13 +6,17 @@ import glob
 from SPACEPRIME import get_data_path
 from scipy.stats import ttest_rel
 from stats import cronbach_alpha
+from stats import remove_outliers
 from SPACEPRIME.subjects import subject_ids
 
 
 df = pd.concat([pd.read_csv(glob.glob(f"{get_data_path()}derivatives/preprocessing/sub-{subject}/beh/sub-{subject}_clean*.csv")[0]) for subject in subject_ids])
+df = df[df["phase"]!=2]
+df = remove_outliers(df, column_name="rt", threshold=2)
 #df = df[df["SingletonPresent"] == 0]
-df_mean = df.groupby(["subject_id", "Priming"])["select_target"].mean().reset_index()
+df_mean = df.groupby(["subject_id", "Priming"])[["select_target", "rt"]].mean().reset_index()
 # plot
+plt.figure()
 barplot = sns.barplot(data=df_mean, x="Priming", y="select_target", errorbar=("se", 1))
 plot_individual_lines(ax=barplot, data=df_mean, y_col="select_target")
 plt.xlabel("Priming")
@@ -22,7 +26,7 @@ barplot.set_xticklabels(["Negative", "No", "Positive"])
 t, pval = ttest_rel(df_mean.query("Priming==0")["rt"].astype(float),
                     df_mean.query("Priming==-1")["rt"].astype(float), nan_policy="omit")
 
-df_pivot = df.groupby(["subject_id", "Priming"])["select_target"].mean().reset_index().pivot(index="subject_id",
-                                                                                             columns="Priming",
-                                                                                             values="select_target").astype(float)
-cronbach_alpha(data=df_pivot)
+#df_pivot = df.groupby(["subject_id", "Priming"])["select_target"].mean().reset_index().pivot(index="subject_id",
+#                                                                                             columns="Priming",
+#                                                                                             values="select_target").astype(float)
+#cronbach_alpha(data=df_pivot)
