770214a12d50c5fd9da9f94998dc9672393f2aa3
Max <schulz.max5@gmail.com>
2025-03-26 16:32:45 +0100

    Plot lateral controls, too

--- SPACEPRIME/passive_listening_ERP.py
+++ SPACEPRIME/passive_listening_ERP.py
@@ -1,52 +1,13 @@
 import mne
 import matplotlib.pyplot as plt
-import glob
-from SPACEPRIME import get_data_path
-from SPACEPRIME.subjects import subject_ids
 from utils import get_passive_listening_ERPs
-import pandas as pd
 from mne.stats import permutation_cluster_test
 from scipy.stats import t
-
 plt.ion()
 
 
-epochs, contra_distractor_epochs_data, ipsi_distractor_epochs_data, contra_target_epochs_data, ipsi_target_epochs_data, diff_target, diff_distractor = get_passive_listening_ERPs()
-
-
-all_evokeds = dict()
-evks_avrgd = dict()
-
-for subject in subject_ids[2:]:
-    epochs_single_sub = mne.read_epochs(glob.glob(f"{get_data_path()}derivatives/epoching/sub-{subject}/eeg/sub-{subject}_task-passive-epo.fif")[0])
-    n_epochs = epochs_single_sub.events.__len__()
-    metadata = pd.DataFrame({"subject": [subject] * n_epochs})
-    epochs_single_sub.metadata = metadata
-    evoked = epochs_single_sub.average(by_event_type=True)
-    for cond in evoked:
-        if cond.comment not in all_evokeds.keys():
-            all_evokeds[cond.comment] = [cond]
-        else:
-            all_evokeds[cond.comment].append(cond)
-    for key in all_evokeds:
-        evks_avrgd[key] = mne.grand_average(all_evokeds[key])
-
-left_target_evks = list()
-right_target_evks = list()
-left_distractor_evks = list()
-right_distractor_evks = list()
-
-for k, v in evks_avrgd.items():
-    if "target-location-1" in k:
-        contra_target_data = v.pick("C4")
-        ipsi_target_data = v.pick("C3")
-        left_target_evks.append(v)
-    elif "target-location-3" in k:
-        right_target_evks.append(v)
-    elif "distractor-location-1" in k:
-        left_distractor_evks.append(v)
-    elif "distractor-location-3" in k:
-        right_distractor_evks.append(v)
+# get all the epochs
+epochs, contra_distractor_epochs_data, ipsi_distractor_epochs_data, contra_target_epochs_data, ipsi_target_epochs_data, contra_control_epochs_data, ipsi_control_epochs_data, diff_target, diff_distractor, diff_control = get_passive_listening_ERPs()
 
 # get time points from epochs
 times =  epochs.times
@@ -54,39 +15,47 @@ times =  epochs.times
 from scipy.stats import ttest_ind
 result_target = ttest_ind(contra_target_epochs_data, ipsi_target_epochs_data, axis=0)
 result_distractor = ttest_ind(contra_distractor_epochs_data, ipsi_distractor_epochs_data, axis=0)
+result_control = ttest_ind(contra_control_epochs_data, ipsi_control_epochs_data, axis=0)
 # plot the data
-fig, ax = plt.subplots(2, 2)
-# first plot
+fig, ax = plt.subplots(2, 3, sharex=True, sharey=False)
+# target plot
 ax[0][0].plot(times, contra_target_epochs_data.mean(axis=0), color="r")
 ax[0][0].plot(times, ipsi_target_epochs_data.mean(axis=0), color="b")
 ax[0][0].plot(times, diff_target[0], color="g")
-ax[0][0].axvspan(0.2, 0.3, color='gray', alpha=0.3)  # Shade the area
-ax[0][0].axvspan(0.05, 0.15, color='gray', alpha=0.3)  # Shade the area
 ax[0][0].hlines(y=0, xmin=times[0], xmax=times[-1])
 ax[0][0].legend(["Contra", "Ipsi", "Contra-Ipsi"])
 ax[0][0].set_title("Target lateral")
 ax[0][0].set_ylabel("Amplitude [µV]")
 ax[0][0].set_xlabel("Time [s]")
-# second plot
+# distractor plot
 ax[0][1].plot(times, contra_distractor_epochs_data.mean(axis=0), color="r")
 ax[0][1].plot(times, ipsi_distractor_epochs_data.mean(axis=0), color="b")
 ax[0][1].plot(times, diff_distractor[0], color="g")
-ax[0][1].axvspan(0.25, 0.50, color='gray', alpha=0.3)  # Shade the area
-ax[0][1].axvspan(0.05, 0.15, color='gray', alpha=0.3)  # Shade the area
 ax[0][1].hlines(y=0, xmin=times[0], xmax=times[-1])
-ax[0][1].set_title("distractor lateral")
+ax[0][1].set_title("Distractor lateral")
 ax[0][1].set_ylabel("Amplitude [µV]")
 ax[0][1].set_xlabel("Time [s]")
-# third plot
+# control plot
+ax[0][2].plot(times, contra_control_epochs_data.mean(axis=0), color="r")
+ax[0][2].plot(times, ipsi_control_epochs_data.mean(axis=0), color="b")
+ax[0][2].plot(times, diff_control[0], color="g")
+ax[0][2].hlines(y=0, xmin=times[0], xmax=times[-1])
+ax[0][2].set_title("Control lateral")
+ax[0][2].set_ylabel("Amplitude [µV]")
+ax[0][2].set_xlabel("Time [s]")
+# stats target plot
 ax[1][0].plot(times, result_target[0])
-ax[1][0].axvspan(0.2, 0.3, color='gray', alpha=0.3)  # Shade the area
-ax[1][0].axvspan(0.05, 0.15, color='gray', alpha=0.3)  # Shade the area
 ax[1][0].hlines(y=0, xmin=times[0], xmax=times[-1])
-# fourth plot
+# stats distractor plot
 ax[1][1].plot(times, result_distractor[0])
-ax[1][1].axvspan(0.25, 0.50, color='gray', alpha=0.3)  # Shade the area
-ax[1][1].axvspan(0.05, 0.15, color='gray', alpha=0.3)  # Shade the area
 ax[1][1].hlines(y=0, xmin=times[0], xmax=times[-1])
+# stats control plot
+ax[1][2].plot(times, result_control[0])
+ax[1][2].hlines(y=0, xmin=times[0], xmax=times[-1])
+# let stats plot share y axes
+ax[1][0].set_ylim((-7, 7))
+ax[1][1].set_ylim((-7, 7))
+ax[1][2].set_ylim((-7, 7))
 plt.tight_layout()
 
 # --- STATISTICS ---
