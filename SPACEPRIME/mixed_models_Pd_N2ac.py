8ee4f0a2a7c87d2aa5ae90cf34edfc021bdf037f
Max Schulz <max.schulz10@web.de>
2025-03-26 09:46:26 +0100

    Avoid exception in pandas

--- SPACEPRIME/mixed_models_Pd_N2ac.py
+++ SPACEPRIME/mixed_models_Pd_N2ac.py
@@ -13,9 +13,10 @@ plt.ion()
 # for a couple of linear mixed models. Overall, we want to aim for a mixed model which contains all possible relevant
 # predictors and dependent variables. Let's go.
 # First, we load up the EEG data, which already includes the behavior as metadata attribute.
-epochs = mne.concatenate_epochs([mne.read_epochs(glob.glob(f"{get_data_path()}derivatives/epoching/sub-{subject}/eeg/sub-{subject}_task-spaceprime-epo.fif")[0], preload=False) for subject in subject_ids])
 # crop to save RAM
-epochs.crop(0, 0.7)
+tmin = 0  # tmin
+tmax = 0.5  # tmax
+epochs = mne.concatenate_epochs([mne.read_epochs(glob.glob(f"{get_data_path()}derivatives/epoching/sub-{subject}/eeg/sub-{subject}_task-spaceprime-epo.fif")[0], preload=True).crop(tmin, tmax) for subject in subject_ids])
 # We want to calculate some average values for N2ac and Pd, therefore, we have to do some data transformation magic.
 # First, we save the dataframe in a separate variable to facilitate modification.
 df = epochs.metadata
@@ -50,6 +51,7 @@ merged_diff_wave_df = pd.merge(grouped_Pd_df, grouped_N2ac_df, on=["epoch", "con
 # Instantiate columns to store Pd and N2ac amplitudes.
 df["N2ac_mean_amplitude"] = None
 df["Pd_mean_amplitude"] = None
+df["Lateral"] = None
 # Iterate over one of the created dataframes
 for i, trial in merged_diff_wave_df.iterrows():
     # retrieve the condition for the current trial
@@ -57,38 +59,42 @@ for i, trial in merged_diff_wave_df.iterrows():
     # check for lateralized targets and subtract contra - ipsi
     if "Target-1" in cond and "Singleton-2" in cond:
         # Now, determine what electrode is contra and ipsi
-        df["N2ac_mean_amplitude"].iloc[i] = trial.FC6 - trial.FC5
-        df["Pd_mean_amplitude"].iloc[i] = None
+        df.loc[df.index[i], "N2ac_mean_amplitude"] = trial.FC6 - trial.FC5
+        df.loc[df.index[i], "Pd_mean_amplitude"] = None
+        df.loc[df.index[i], "Lateral"] = 1
     elif "Target-3" in cond and "Singleton-2" in cond:
-        df["N2ac_mean_amplitude"].iloc[i] = trial.FC5 - trial.FC6
-        df["Pd_mean_amplitude"].iloc[i] = None
+        df.loc[df.index[i], "N2ac_mean_amplitude"] = trial.FC5 - trial.FC6
+        df.loc[df.index[i], "Pd_mean_amplitude"] = None
+        df.loc[df.index[i], "Lateral"] = 1
     # check for lateralized distractors and subtract contra - ipsi
     elif "Singleton-1" in cond and "Target-2" in cond:
-        df["Pd_mean_amplitude"].iloc[i] = trial.C4 - trial.C3
-        df["N2ac_mean_amplitude"].iloc[i] = None
+        df.loc[df.index[i], "Pd_mean_amplitude"] = trial.C4 - trial.C3
+        df.loc[df.index[i], "N2ac_mean_amplitude"] = None
+        df.loc[df.index[i], "Lateral"] = 2
     elif "Singleton-3" in cond and "Target-2" in cond:
-        df["Pd_mean_amplitude"].iloc[i] = trial.C3 - trial.C4
-        df["N2ac_mean_amplitude"].iloc[i] = None
+        df.loc[df.index[i], "Pd_mean_amplitude"] = trial.C3 - trial.C4
+        df.loc[df.index[i], "N2ac_mean_amplitude"] = None
+        df.loc[df.index[i], "Lateral"] = 2
     # insert None in case none of the above conditions are satisfied
-    else:
-        df["N2ac_mean_amplitude"].iloc[i] = None
-        df["Pd_mean_amplitude"].iloc[i] = None
+    elif "Singleton-0" in cond:
+        df.loc[df.index[i], "N2ac_mean_amplitude"] = None
+        df.loc[df.index[i], "Pd_mean_amplitude"] = None
+        df.loc[df.index[i], "Lateral"] = None
 # Now that we have all the variables we need, we can do some statistical modeling.
 # First, we clean up the dataframe
-df.drop("duration", axis=1, inplace=True)  # drop duration because it is always NaN
 formula = "Pd_mean_amplitude ~ SingletonLoc"
 # some formatting
-pd_df = df[["Pd_mean_amplitude", "SingletonLoc", "subject_id"]].dropna(subset=["Pd_mean_amplitude", "SingletonLoc"],
+pd_stat_df = df[["Pd_mean_amplitude", "SingletonLoc", "subject_id"]].dropna(subset=["SingletonLoc"],
                                                                        ignore_index=True).astype(float)
-model = smf.mixedlm(formula=formula, data=pd_df, groups="subject_id")
+model = smf.mixedlm(formula=formula, data=pd_stat_df, groups="subject_id")
 result = model.fit()
 result.summary()
-sns.lmplot(data=pd_df, x="SingletonLoc", y="Pd_mean_amplitude", hue="subject_id")
+sns.lmplot(data=pd_stat_df, x="SingletonLoc", y="Pd_mean_amplitude", hue="subject_id")
 # Modeling N2ac
 formula = "N2ac_mean_amplitude ~ TargetLoc"
 # some formatting
-n2ac_df = df[["N2ac_mean_amplitude", "TargetLoc", "subject_id"]].dropna(subset=["N2ac_mean_amplitude", "TargetLoc"],
+n2ac_stat_df = df[["N2ac_mean_amplitude", "TargetLoc", "subject_id"]].dropna(subset=["N2ac_mean_amplitude", "TargetLoc"],
                                                                        ignore_index=True).astype(float)
-model = smf.mixedlm(formula=formula, data=n2ac_df, groups="subject_id")
+model = smf.mixedlm(formula=formula, data=n2ac_stat_df, groups="subject_id")
 result = model.fit()
 result.summary()
