68245d5514d583f24a3acf475a2c17bc4817ff8d
Max <schulz.max5@gmail.com>
2025-04-01 11:53:20 +0200

    Update WIP

--- SPACEPRIME/alpha_power_lateralization_pre-stim.py
+++ SPACEPRIME/alpha_power_lateralization_pre-stim.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 from SPACEPRIME.plotting import plot_individual_lines
+from mne.stats import permutation_cluster_1samp_test
 plt.ion()
 
 
@@ -16,9 +17,9 @@ plt.ion()
 # We first retrieve our epochs
 epochs = mne.concatenate_epochs([mne.read_epochs(glob.glob(f"{get_data_path()}derivatives/epoching/sub-{subject}/eeg/sub-{subject}_task-spaceprime-epo.fif")[0], preload=False) for subject in subject_ids])
 # Control for priming
-# epochs = epochs["Priming==0"]
+#epochs = epochs["Priming==0"]
 # crop to reduce runtime
-epochs.crop(None, 0.2)
+# epochs.crop(None, 0)
 # First, define ROI electrode picks to reduce computation time.
 left_roi = ["TP9", "TP7", "CP5", "CP3", "CP1", "P7", "P5", "P3", "P1", "PO7", "PO3", "O1"]
 right_roi = ["TP10", "TP8", "CP6", "CP4", "CP2", "P8", "P6", "P4", "P2", "PO8", "PO4", "O2"]
@@ -34,26 +35,26 @@ freqs = numpy.arange(1, 31, 1)  # 1 to 30 Hz
 #window_length = 0.5  # window lengths as in Wöstmann et al. (2019)
 n_cycles = freqs / 2  # different number of cycle per frequency
 method = "morlet"  # wavelet
-decim = 7  # keep only every fifth of the samples along the time axis
-mode = "zscore"  # z-score normalization
-baseline = (-0.9, -0.5)  # baseline from 1000 to 500 ms pre-stimulus
+decim = 10  # keep only every fifth of the samples along the time axis
+mode = "logratio"  # z-score normalization
+baseline = None  # Do not use baseline interval
 n_jobs = -1  # number of parallel jobs. -1 uses all cores
 average = False  # get total oscillatory power, opposed to evoked oscillatory power (get power from ERP)
 # Compute time-frequency analysis
-power = epochs.compute_tfr(method=method, freqs=freqs, n_cycles=n_cycles, average=average, n_jobs=n_jobs, decim=decim)
-# apply baseline
-# power.apply_baseline(baseline=baseline, mode=mode)
+power_total = epochs.compute_tfr(method=method, freqs=freqs, n_cycles=n_cycles, average=average, n_jobs=n_jobs, decim=decim)
+# apply baseline --> actually only zscore without baselining
+#power.apply_baseline(baseline=baseline, mode=mode)
 # Furthermore, since we are interested in the induced alpha power only, we get the evoked alpha by averagring epochs and
 # then conducting TFR analysis. We subtract the evoked power from the total power to get induced oscillatory power.
 power_evoked = epochs.average().compute_tfr(method=method, freqs=freqs, decim=decim, n_cycles=n_cycles, n_jobs=n_jobs)
 # apply baseline
-# power_evoked.apply_baseline(baseline=baseline, mode=mode)
+#power_evoked.apply_baseline(baseline=baseline, mode=mode)
 # Subtract the evoked power, trial by trial.
-power_induced = power.copy()
-for trial in range(len(power)):
-    power_induced.data[trial] = power.data[trial] - power_evoked.data[0]
+power_induced = power_total.copy()
+for trial in range(len(power_total)):
+    power_induced.data[trial] -= power_evoked.data[0]  # subtract the evoked power from total power
 # apply baseline
-# power_induced.apply_baseline(baseline, mode=mode)
+power_induced.apply_baseline(baseline, mode=mode)
 # Now, we devide the epochs into our respective priming conditions. In order to do so, we make use of the metadata
 # which was appended to the epochs of every subject during preprocessing. We can access the metadata the same way as
 # we would by calling the event_ids in the experiment.
@@ -65,11 +66,21 @@ power_incorrects = power_induced["select_target==False"]
 power_diff = power_corrects.average() - power_incorrects.average()
 power_diff.plot(combine="mean")
 
+# The difference in induced oscillatory power between correct and incorrect trials we observe in the data might be
+# predictive of the performance of the participants. In order to find out where this power difference originates from
+# (broadly), we can apply a 1-sample
+# permutation cluster test. MNE provides a pipeline for this procedure, we just have to apply it to our own data.
+sensor_adjacency, ch_names = mne.channels.find_ch_adjacency(power_diff.info, "eeg")
+
+
+
+
+
 # define some params for the upcoming analysis
 alpha_fmin = 8
 alpha_fmax = 12
-alpha_tmin = -0.3
-alpha_tmax = 0.0
+alpha_tmin = None
+alpha_tmax = None
 # Store subject-level results
 subject_results = {}
 for subject in subject_ids:
@@ -108,10 +119,11 @@ df = pd.DataFrame({
 # plot the stuff
 plot = sns.barplot(x="condition", y="alpha", data=df)
 plot_individual_lines(plot, data=df, x_col="condition", y_col="alpha")
-plt.title("Subject-Level Alpha Power (7-14 Hz)")
-plt.ylabel("Alpha Power z-score")
+plt.title("Subject-Level Total Alpha Power (8-12 Hz)")
+plt.ylabel("Alpha Power")
 plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
+plt.legend("")
 # Subtract correct from incorrect alpha
 diff = df.query("condition=='Correct'")["alpha"].reset_index(drop=True) - df.query("condition=='Incorrect'")["alpha"].reset_index(drop=True)
 # Compute t test
@@ -120,7 +132,7 @@ t, p = stats.ttest_1samp(diff, popmean=0)
 # Okay, so now that we have computed the overall alpha power time courses and made a simple comparison between correct
 # and incorrect trials, we can further divide the alpha power into ipsi- and contralateral ROIs. This might be more
 # sensitive than looking at overall alpha power.
-all_conds = list(power.event_id.keys())
+all_conds = list(power_total.event_id.keys())
 # equalize epoch count in all conditions
 #mne.epochs.equalize_epoch_counts([left_target_epochs_correct, right_target_epochs_correct,
                                   #left_target_epochs_incorrect, right_target_epochs_incorrect], method="random")
@@ -211,7 +223,7 @@ df_melted[['condition', 'side']] = df_melted['condition_side'].str.split('_', ex
 plt.figure()
 sns.boxplot(x='condition', y='value', hue='side', data=df_melted)
 plt.hlines(y=0, xmin=plt.xlim()[0], xmax=plt.xlim()[1], linestyles="dashed", color="black")
-plt.title('Boxplots by Condition and Side')
+plt.title('Alpha power lateralization (Contra - Ipsi)')
 plt.xlabel('Condition')
 plt.ylabel('Value')
 
@@ -236,8 +248,8 @@ t, p = stats.ttest_rel(diff_correct, diff_incorrect)
 # Store the computed data in a dataframe
 alpha_lateralization_subjects = dict()
 # Split into left and right lateral targets
-left_target_power = power_induced[[x for x in power.event_id if "Target-1-Singleton-2" in x]]
-right_target_power = power_induced[[x for x in power.event_id if "Target-3-Singleton-2" in x]]
+left_target_power = power_induced[[x for x in power_total.event_id if "Target-1-Singleton-2" in x]]
+right_target_power = power_induced[[x for x in power_total.event_id if "Target-3-Singleton-2" in x]]
 # Now, divide into correct and incorrect trials
 left_target_power_correct = left_target_power["select_target==True"]
 right_target_power_correct = right_target_power["select_target==True"]
@@ -282,7 +294,7 @@ alpha_lateralization_subjects["incorrect_contra"] = contra_target_power_incorrec
 alpha_lateralization_subjects["correct_contra"] = contra_target_power_correct_data
 
 # Plot single subject and grand average data
-times = power.times
+times = power_induced.times
 incorrect_diff_sub = alpha_lateralization_subjects["incorrect_contra"] - alpha_lateralization_subjects["incorrect_ipsi"]
 correct_diff_sub = alpha_lateralization_subjects["correct_contra"] - alpha_lateralization_subjects["correct_ipsi"]
 plt.plot(times, incorrect_diff_sub,
@@ -292,71 +304,3 @@ plt.plot(times, correct_diff_sub,
 plt.plot(times, (correct_diff_sub-incorrect_diff_sub), color="black",
          label=f"Total alpha lateralization")
 plt.legend()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# iterate over subjects
-# --- DO ALPHA POWER LATERALIZATION CALCULATION FOR ALL PRIMING CONDITIONS AND SUBJECTS ---
-for subject in subject_ids:
-    # no priming
-    # compute within-subject alpha power
-    power_no_p_ipsi = no_p[f"subject_id=={subject}&TargetLoc==1"].compute_tfr(method=method, freqs=freqs, n_cycles=n_cycles, decim=decim, n_jobs=-1, return_itc=False,
-                           average=False).get_data(picks=)
-    power_avg_no_p = power_no_p.average()
-    alpha_freqs_times_no_p = power_avg_no_p.get_data(fmin=7, fmax=14).mean(axis=0)  # get alpha power frequencies over all channels
-    subjects_alpha["no_p"].append(alpha_freqs_times_no_p.mean(axis=0))  # average over all frequencies
-
-    # positive priming
-    power_pp = pp[f"subject_id=={subject}"].compute_tfr(method=method, freqs=freqs, n_cycles=n_cycles, decim=decim, n_jobs=-1, return_itc=False,
-                           average=False)
-    power_avg_pp = power_pp.average()
-    alpha_freqs_times_pp = power_avg_pp.get_data(fmin=7, fmax=14).mean(axis=0)  # get alpha power frequencies over all channels
-    subjects_alpha["pp"].append(alpha_freqs_times_pp.mean(axis=0))  # average over all frequencies
-
-    # negative priming
-    power_np = np[f"subject_id=={subject}"].compute_tfr(method=method, freqs=freqs, n_cycles=n_cycles, decim=decim, n_jobs=-1, return_itc=False,
-                           average=False)
-    power_avg_np = power_np.average()
-    alpha_freqs_times_np = power_avg_np.get_data(fmin=7, fmax=14).mean(axis=0)  # get alpha power frequencies over all channels
-    subjects_alpha["np"].append(alpha_freqs_times_np.mean(axis=0))  # average over all frequencies
-
-# get the time vector for plotting purposes
-times = numpy.linspace(epochs.tmin, epochs.tmax, epochs.get_data().shape[2])
-
-# plot data
-plt.figure()
-for k, v in subjects_alpha.items():
-    for i, subject_alpha in enumerate(v):
-        plt.plot(times, subject_alpha, label=f"Condition: {k}, Subject {subject_ids[i]}", alpha=0.2)
-
-# get average of subjects alpha
-grand_average_no_p = numpy.mean(subjects_alpha["no_p"], axis=0)
-grand_average_pp = numpy.mean(subjects_alpha["pp"], axis=0)
-grand_average_np = numpy.mean(subjects_alpha["np"], axis=0)
-
-plt.plot(times, grand_average_no_p, label="Grand Average no priming", color="grey", linewidth=5)
-plt.plot(times, grand_average_pp, label="Grand Average positive priming", color="darkgreen", linewidth=5)
-plt.plot(times, grand_average_np, label="Grand Average negative priming", color="darkred", linewidth=5)
-
-plt.xlabel("Time (s)")
-plt.ylabel("Alpha Power")
-plt.title("Time Course of Alpha Power")
-plt.legend()
-plt.show()
-
-# Since we cannot observe anything in total alpha power, we might look at how the lateralization differs between
-# priming conditions. To do this, we define ROIs and specifically look at how alpha lateralizes in no priming, positive
-# priming and negative priming conditions.
