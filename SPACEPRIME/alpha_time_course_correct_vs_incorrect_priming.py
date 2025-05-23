8d1f98e45c4de7ce9a606bd59717009aed74bc10
Max <schulz.max5@gmail.com>
2025-05-23 10:08:23 +0200

    Alpha diff (correct - incorrect) as a function of priming

--- SPACEPRIME/alpha_time_course_correct_vs_incorrect_priming.py
+++ SPACEPRIME/alpha_time_course_correct_vs_incorrect_priming.py
@@ -9,11 +9,10 @@ plt.ion()
 
 
 # load up the epochs from all subjects
-epochs = mne.concatenate_epochs([mne.read_epochs(glob.glob(f"{get_data_path()}derivatives/epoching/sub-{subject}/eeg/sub-{subject}_task-spaceprime-epo.fif")[0], preload=False) for subject in subject_ids])
 # First, define ROI electrode picks to reduce computation time.
 left_roi = ["TP9", "TP7", "CP5", "CP3", "CP1", "P7", "P5", "P3", "P1", "PO7", "PO3", "O1"]
 right_roi = ["TP10", "TP8", "CP6", "CP4", "CP2", "P8", "P6", "P4", "P2", "PO8", "PO4", "O2"]
-epochs = epochs.pick(picks=left_roi+right_roi)
+epochs = mne.concatenate_epochs([mne.read_epochs(glob.glob(f"{get_data_path()}derivatives/epoching/sub-{subject}/eeg/sub-{subject}_task-spaceprime-epo.fif")[0], preload=True).pick(left_roi+right_roi) for subject in subject_ids])
 # define some TFR params
 # define alpha frequency range
 alpha_freqs = np.arange(8, 13, 1)
@@ -21,13 +20,14 @@ n_cycles = alpha_freqs / 2  # different number of cycle per frequency
 #window_length = 0.5  # window lengths as in Wöstmann et al. (2019)
 method = "morlet"  # wavelet
 decim = 5  # keep all the samples along the time axis
-mode = "mean"
-baseline = (None, None)
+n_jobs = 5
+#mode = "mean"
+#baseline = (None, None)
 # compute the absolute oscillatory power for all subjects
 alpha_power = epochs.compute_tfr(method=method, freqs=alpha_freqs, n_cycles=n_cycles,
-                           decim=decim, n_jobs=-1, return_itc=False, average=False)
+                           decim=decim, n_jobs=n_jobs, return_itc=False, average=False)
 # apply baseline to transform the y-axis into dB values
-alpha_power.apply_baseline(baseline=baseline, mode=mode)
+# alpha_power.apply_baseline(baseline=baseline, mode=mode)
 # store the within-subject alpha power
 subjects_alpha = dict(np=dict(),
                       pp=dict(),
@@ -77,6 +77,7 @@ plt.xlabel("Time (s)")
 plt.ylabel("Alpha Power")
 plt.title("Alpha Time Course")
 sns.despine(fig=fig)
+
 
 # plot topography of alpha
 alpha_power.average().plot_topomap(tmin=0, tmax=0.75)
