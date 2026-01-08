7605b0b3f48cbe94a4a5109ab92f72aeb910f3a4
Max <schulz.max5@gmail.com>
2026-01-08 16:54:10 +0100

    Deletes obsolete SPACEPRIME analysis scripts.

--- SPACEPRIME/time_align_epochs.py
+++
@@ -1,7 +0,0 @@
-import mne
-
-
-subject = 104
-epochs = mne.read_epochs(f"/home/max/Insync/schulz.max5@gmail.com/GoogleDrive/PhD/data/SPACEPRIME/derivatives/epoching/sub-{subject}/eeg/sub-{subject}_task-flanker-epo.fif",
-                                   preload=True)
-aligned_epochs = epochs.copy().shift_time(tshift=-0.05)
